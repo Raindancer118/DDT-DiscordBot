7e8ae7dfda062f92a47b55e3492865cbb26cab3e
Raindancer118 <tom560stieh@gmail.com>
2025-12-15 20:34:56 +0100

    feat: add new `login` command and its handler

--- src/commands/index.js
+++ src/commands/index.js
@@ -1,11 +1,13 @@
 import { definition as dhDefinition, handle as handleDh } from './dh.js';
 import { definition as clearDefinition, handle as handleClear } from './clear.js';
 import { definition as statusDefinition, handle as handleStatus } from './status.js';
+import { definition as loginDefinition, handle as handleLogin } from './login.js';
 
-export const commandDefinitions = [dhDefinition, clearDefinition, statusDefinition];
+export const commandDefinitions = [dhDefinition, clearDefinition, statusDefinition, loginDefinition];
 
 export const commandHandlers = {
   dh: handleDh,
   clear: handleClear,
-  status: handleStatus
+  status: handleStatus,
+  login: handleLogin
 };
