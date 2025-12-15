62411cc00eb34d268e255085b2afd4d3a05c5fb2
Raindancer118 <tom560stieh@gmail.com>
2025-12-15 20:52:59 +0100

    refactor: Adjust `App.Locals` type definition and add Discord client secret and redirect URI configuration instructions.

--- src/env.d.ts
+++ src/env.d.ts
@@ -10,5 +10,7 @@ type Env = {
 type Runtime = import('@astrojs/cloudflare').Runtime<Env>;
 
 declare namespace App {
-    interface Locals extends Runtime { }
+    interface Locals {
+        runtime: Runtime;
+    }
 }
